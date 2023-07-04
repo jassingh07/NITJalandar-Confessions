576d443e5ebe5a0e926ce61428f8adf4ba2aaf7f
Jaskeerat <erjaskeeratsingh@gmail.com>
2023-07-04 20:09:07 +0530

    used passport to create sessions-> they use hashing + salting inbuilt

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const bcrypt = require("bcrypt");
-const saltRounds = Number(process.env.saltRounds);
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 // console.log(test);
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session()); //use passport to deal with session
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
@@ -24,42 +35,21 @@ const userSchema = mongoose.Schema({
   username: String,
   password: String,
 });
+userSchema.plugin(passportLocalMongoose);
+//passport will hash and salt passwords for us
 
-//encryption applied to password field
+const User = mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser()); //create cookie for user identity
+passport.deserializeUser(User.deserializeUser()); //decrypt information from cookie to verify user's identity
+
 //encryption done because in database password was clearly visible which we don't want if multiple people
 //can access the database
-const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-app
-  .route("/login")
-  .get((req, res) => {
-    res.render("login");
-  })
-  .post((req, res) => {
-    // console.log(req.body);
-    User.findOne({ username: req.body.username })
-      .then((result) => {
-        if (result) {
-          bcrypt
-            .compare(req.body.password, result.password)
-            .then((matchFound) => {
-              if (matchFound) res.render("secrets");
-              else res.send("Incorrect Password entry, plz try again");
-            });
-        } else {
-          res.send("Email entered was incorrect, plz try again!!");
-        }
-      })
-      .catch((err) => {
-        res.send(
-          "Server Error: Unable to login currently, plz try again later"
-        );
-      });
-  });
 
 app
   .route("/register")
@@ -67,28 +57,54 @@ app
     res.render("register");
   })
   .post((req, res) => {
-    bcrypt
-      .hash(req.body.password, saltRounds)
-      .then((hash) => {
-        const newUser = new User({
-          username: req.body.username,
-          password: hash,
+    User.register({ username: req.body.username }, req.body.password)
+      .then(() => {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
         });
-        // console.log(req.body);
-        newUser
-          .save()
-          .then((result) => {
-            res.render("secrets");
-          })
-          .catch((err) => {
-            res.send(
-              "Server Error: Unable to register currently, plz try again later!!"
-            );
-          });
       })
       .catch((err) => {
-        res.send(
-          "Server Error: Unable to provide Secure registration at the moment, plz try again later!!"
-        );
+        console.log(err);
+        res.redirect("/register");
       });
   });
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const user = new User(req.body);
+    req.login(user, function(err) {
+      if (err) { 
+        console.log(err);
+        res.redirect("/login");
+      }
+      else{
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    });
+  });
+
+
+  app.route("/logout")
+  .get((req, res) => {
+    req.logout( err => {
+      if (err) { 
+        console.log(err);
+        res.redirect("/secrets");
+      }
+      res.redirect('/');
+    });
+  });
+
+app.route("/secrets").get((req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
